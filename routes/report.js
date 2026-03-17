249eedf8f3433ca82c864fa8560006e280cbb077
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-03-17 10:08:50 -0400

    Connected app to call lambda and send report

--- routes/report.js
+++ routes/report.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { InvokeCommand } = require("@aws-sdk/client-lambda");
+const { LambdaClient } = require("@aws-sdk/client-lambda");
+const lambda = new LambdaClient({ region: "us-east-1" });
 
-router.post('/reading-history', (req, res) => {
+
+
+router.post('/reading-history', async (req, res) => {
+  const functionName = "project_lambda_function";
   const { reportType } = req.body;
 
-  console.log("Report type:", reportType);
-  console.log("User:", req.session.userId);
-  
-  res.send('Reading history report job started.');
+  try {
+    const command = new InvokeCommand({
+      FunctionName: functionName,
+      Payload: Buffer.from(JSON.stringify({
+        reportType: reportType,
+        userId: req.session.userId
+      }))
+    });
+
+    await lambda.send(command);
+    res.send('Reading history report job started.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error starting report job.');
+  }  
 });
 
 module.exports = router;
